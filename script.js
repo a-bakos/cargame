c53c844b57788bc25b4d82ccd9a178043b1206c0
a-bakos <bakosattila@fastem.com>
2017-02-20 15:52:56 +0000

    first tests

--- script.js
+++ script.js
@@ -5,10 +5,25 @@ var vehicleHeadLightsRight = document.querySelector(".headlights-right");
 var vehicleTailLightsLeft = document.querySelector(".taillights-left");
 var vehicleTailLightsRight = document.querySelector(".taillights-right");
 
-addEventListener("click", function(event) {
-  vehicleHeadLightsLeft.classList.toggle('vehicle-headlights_on');
-  vehicleHeadLightsRight.classList.toggle('vehicle-headlights_on');
-  vehicleTailLightsLeft.classList.toggle('vehicle-taillights_on');
-  vehicleTailLightsRight.classList.toggle('vehicle-taillights_on');
-  vehicle.classList.toggle('vehicle-lights_on');
+// Turn the light on and off
+addEventListener("keydown", function(event) {
+  if (event.keyCode == 76) {
+    vehicleHeadLightsLeft.classList.toggle('vehicle-headlights_on');
+    vehicleHeadLightsRight.classList.toggle('vehicle-headlights_on');
+    vehicleTailLightsLeft.classList.toggle('vehicle-taillights_on');
+    vehicleTailLightsRight.classList.toggle('vehicle-taillights_on');
+    vehicle.classList.toggle('vehicle-lights_on');
+    console.log("Lights turned on/off.");
+  }
+  
+  if (event.keyCode == 38) {
+    console.log("Move left.");
+    vehicle.className = "vehicle";
+    vehicle.classList.toggle("move-left");
+  }
+  if (event.keyCode == 40) {
+    console.log("Move right.");
+    vehicle.className = "vehicle";
+    vehicle.classList.toggle("move-right");
+  }
 });
