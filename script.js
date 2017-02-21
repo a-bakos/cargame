14a6c347ca12e41d8233e25d1a5352f70405ac04
a-bakos <bakosattila@fastem.com>
2017-02-21 16:21:09 +0000

    vehicle lights

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ var vehicleHeadLightsLeft = document.querySelector(".headlights-left");
 var vehicleHeadLightsRight = document.querySelector(".headlights-right");
 var vehicleTailLightsLeft = document.querySelector(".taillights-left");
 var vehicleTailLightsRight = document.querySelector(".taillights-right");
+var vehicleIndicatorsLeft = document.querySelector(".indicator-left");
+var vehicleIndicatorsRight = document.querySelector(".indicator-right");
 
 // Turn the light on and off
 addEventListener("keydown", function(event) {
@@ -16,14 +18,33 @@ addEventListener("keydown", function(event) {
     console.log("Lights turned on/off.");
   }
   
+  // Move left -- that is, move upwards on screen
   if (event.keyCode == 38) {
     console.log("Move left.");
     vehicle.className = "vehicle";
     vehicle.classList.toggle("move-left");
   }
+  // Move right -- that is, move downwards on screen
   if (event.keyCode == 40) {
     console.log("Move right.");
     vehicle.className = "vehicle";
     vehicle.classList.toggle("move-right");
   }
+
+  // Indicator light controls
+  if (event.keyCode == 81) {
+    console.log("Left indicator lights.");
+    vehicleIndicatorsLeft.classList.toggle("vehicle-indicators_on");
+  }
+  if (event.keyCode == 69) {
+    console.log("Right indicator lights.");
+    vehicleIndicatorsRight.classList.toggle("vehicle-indicators_on");
+  }
+  if (event.keyCode == 87) {
+    console.log("All indicator lights.");
+    vehicleIndicatorsLeft.classList.toggle("vehicle-indicators_on");
+    vehicleIndicatorsRight.classList.toggle("vehicle-indicators_on");
+  }
+
 });
+
